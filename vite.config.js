fd66d54e992a352b366a6618bc0805207ecc0b51
Igor Mitrović <88263951+eegor111@users.noreply.github.com>
2023-09-21 15:50:27 +0200

    m

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@app', replacement: path.resolve(__dirname, 'src/app') }],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 })
