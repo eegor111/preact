b163cce75febe7b07eeb591b32632e42a687a854
Igor Mitrović <88263951+eegor111@users.noreply.github.com>
2023-11-06 08:30:32 +0100

    m

--- src/app/layout/MainLayout.jsx
+++ src/app/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 // Layout.js
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from '@app/components';
+import Footer from '@app/components';
 
 const Layout = ({ children }) => {
   return (
